80d2d94a3bf9e6cda47d452d866339466c429189
lhfmartin <44720140+lhfmartin@users.noreply.github.com>
2024-03-06 00:25:46 +0800

    Update readme_to_html.py

--- readme_to_html.py
+++ readme_to_html.py
@@ -8,9 +8,9 @@ import requests
 with open(args.i, "r") as f:
     readme = f.read()
 
-r = requests.post("https://api.github.com/markdown", json={"text": readme})
+r = requests.post("https://api.github.com/markdown", json={"text": readme, "mode": "gfm"})
 
-html = "<link href=\"https://unpkg.com/@primer/css/dist/primer.css\" rel=\"stylesheet\" />\n<div class=\"markdown-body\">\n" + r.text + "</div>"
+html = "<link href=\"https://unpkg.com/@primer/css/dist/primer.css\" rel=\"stylesheet\" />\n<div class=\"markdown-body\">\n" + r.text + "\n</div>"
 
 with open(args.o, "w") as f:
     f.write(html)
